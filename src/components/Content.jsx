9ceb97e3d18184bcce0e3f2846fdbb6ff1b5480d
Gizele Santos <gizele.ccardoso@gmail.com>
2023-10-17 17:43:41 -0300

    fix: Add mulheres in base_url

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -14,7 +14,7 @@ export function Content() {
   const [citacao, setCitacao] = useState('')
   const [imagem, setImagem] = useState('')
   const [success, setSuccess] = useState(false)
-  const baseURL = 'https://backend-node-programaria.onrender.com'
+  const baseURL = 'https://backend-node-programaria.onrender.com/mulheres'
 
   useEffect(() => {
     async function getData() {
